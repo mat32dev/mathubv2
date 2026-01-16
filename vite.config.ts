c85a3f435ef9d2509ef1d60c748ef20c3620867a
mat32dev <hola@mat32.com>
2026-01-16 20:00:28 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {x
+  build: {
     target: 'esnext',
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   }
 });
